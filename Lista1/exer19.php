d62cc3a738c8bcf621bd945d3bbb0ba151a4e61a
ElianaSFernandes <elianadesouzafernandes@gmail.com>
2025-02-27 20:51:58 -0300

    Correção do número do exercício 18

--- Lista1/exer19.php
+++ Lista1/exer19.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Exercício 18!</h1>
+    <h1>Exercício 19!</h1>
     
     <form method="post" action="exer18resposta.php">
                         
