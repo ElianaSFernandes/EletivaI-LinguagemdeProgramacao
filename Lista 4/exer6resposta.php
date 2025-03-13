09e338e64798308a20340793fb6f590b800b0d85
ElianaSFernandes <elianadesouzafernandes@gmail.com>
2025-03-13 11:08:40 -0300

    Exercício 6 Lista 4

---
+++ Lista 4/exer6resposta.php
@@ -0,0 +1,42 @@
+<?php
+    declare(strict_types = 1); //OBRIGA QUE TODAS AS FUNÇÕES E VARIÁVEIS TENHAM TIPO DEFINIDO
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    
+    <h1>Resposta do Exercício 6 da Lista 4</h1>
+  
+    <?php
+
+        function numInteiro (float $numero): float {
+            return round ($numero, 0);
+        } 
+             
+        if ($_SERVER ['REQUEST_METHOD'] == "POST"){
+            try {
+                $numero = floatval ($_POST['numero']);                
+                
+                echo ("O valor inteiro do $numero é: " .numInteiro ($numero));
+                          
+            }catch (Exception $e){
+                echo "Erro: ".$e ->getMessage();
+            }
+        }
+    ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+Crie um programa em PHP que recebe um número de ponto flutuante
+e retorna o número arredondado.
